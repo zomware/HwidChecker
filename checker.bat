8d1a88a808a8941e3ddd2b6bd6b586c5a794b14a
zomware <86991444+zomware@users.noreply.github.com>
2024-05-01 19:46:28 -0500

    Create checker.bat

---
+++ checker.bat
@@ -0,0 +1,116 @@
+@echo off
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+rem HWID DETAILS + MORE \\ !recheck to RECHECK DETAILS \\  !exit TO CLOSE PROGRAM (OR JUST USE THE X)
+
+
+
+rem start, join discord for more tools like spoofers, cheetos, etc
+:loader
+
+color B
+
+echo HwidChecker Blitz Solutions - @zomware
+set /p enter="Type 'enter' to load into the checker."
+echo.
+if "%enter%"=="enter" goto main
+echo.
+
+
+
+:main
+cls
+color 5
+for /f "tokens=2 delims==" %%b in ('wmic bios get serialnumber /value') do set "serial=%%b"
+echo Serial Number: %serial%
+
+
+echo System Specifications:
+systeminfo | findstr /C:"System Type" /C:"Total Physical Memory" /C:"Available Physical Memory" /C:"Virtual Memory: Max Size" /C:"Virtual Memory: Available" /C:"Virtual Memory: In Use"
+
+
+echo BIOS Information:
+wmic bios get name, version, manufacturer
+
+
+echo Motherboard Information:
+wmic baseboard get product, manufacturer, serialnumber
+
+
+echo SMBIOS Information:
+wmic path Win32_ComputerSystemProduct get IdentifyingNumber,UUID
+
+
+echo Physical Address and Transport Name:
+ipconfig /all | findstr /C:"Physical Address" /C:"Media State"
+
+set /p command="[!recheck to recheck details] [!exit to exit] "
+if "%command%"=="!recheck" goto main
+if "%command%"=="!exit" break
